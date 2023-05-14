50da93df8a81f615e06e0ceee0de9f7f816539e5
Paloma Ania Benito <palomaaniabenito@MacBook-Air-de-Paloma-2.local>
2023-05-14 16:39:55 +0200

    arreglar que no se mostrara lo oculto al cargar página, que no sé por qué me había dejado de funcionar y antes funcionaba

--- main.js
+++ main.js
@@ -23,69 +23,58 @@ function validate(e) {
 // ********************** JAVASCRIPT de misa.html *********************
 
 
-        // Ponemos el display none para que al cargar la página agarre de primeras
-        // el none, porque sino, al principio, la primera vez tenía que pinchar dos
-        // veces el botón para que comenzara el toggle de la invitación, y con esto
-        // ya es directamente pinchando una vez y funciona perfecto.
-        let estiloNone = document.getElementsByClassName('estilo-none');
-        invisible();
-        function invisible(){
-            for(let i=0; i<estiloNone.length; i++){
-                estiloNone[i].style.display = 'none';
-            }
-        }
+// Ponemos el display none para que al cargar la página agarre de primeras
+// el none, porque sino, al principio, la primera vez tenía que pinchar dos
+// veces el botón para que comenzara el toggle de la invitación, y con esto
+// ya es directamente pinchando una vez y funciona perfecto.
+function invisible(){
+    let estiloNone = document.getElementsByClassName('estilo-none');
+    for(let i=0; i<estiloNone.length; i++){
+        estiloNone[i].style.display = 'none';
+    }
+}
+window.onload = invisible;
 
-        let footer = document.getElementById('footer');
-        footer.style.display = 'none';
 
-        /*
+/*
         
-        FUNCIÓN que hace aparecer y desaperecer los elementos cuando se pincha el botón
+FUNCIÓN que hace aparecer y desaperecer los elementos cuando se pincha el botón
         
-        Se pasa como PARÁMETRO un número que corresponde al índice de la clase 'estilo-none'
-            ● 0 → iglesia
-            ● 1 → restaurante
+Se pasa como PARÁMETRO un número que corresponde al índice de la clase 'estilo-none'
+    ● 0 → iglesia
+    ● 1 → restaurante
 
-        */
-        function aparecerYdesaparecer(elemento) {
-            // Localizar los elementos del html con clase 'estilo-none'
-            let elementos = document.getElementsByClassName('estilo-none');
-            // Bucle para recorrer la clase
-            for(let i=0; i<elementos.length; i++){
-                // Si el botón que se ha seleccionado coincide con el índice que se está recorriendo
-                if(elemento == i){
-                    // Si está es invisible entonces mostrarlo
-                    if (elementos[i].style.display === "none") {
-                        elementos[i].style.display = "block";
-                        console.log(`Aparece ${elemento}`);
-                        // Mostrar footer
-                        footer.style.display = 'inline-block';
-                    // Si es visible entonces ocultarlo
-                    // Esto es para cuando se pincha dos veces seguidas el mismo botón,
-                    // para que se oculte si se estaba mostrando, que queda mejor así.
-                    } else {
-                        elementos[i].style.display = "none";
-                        console.log(`Desaparece ${elemento}`);
-                        // Ocultar footer
-                        footer.style.display = 'none';
-                    }
-                // Si el botón que se ha seleccionado NO coincide con el índice que se está recorriendo
-                // entonces esos elementos se ocultarán siempre.
-                }else{
-                    elementos[i].style.display = "none";
-                }
+*/
+function aparecerYdesaparecer(elemento) {
+    // Localizar los elementos del html con clase 'estilo-none'
+    let elementos = document.getElementsByClassName('estilo-none');
+    // Bucle para recorrer la clase
+    for(let i=0; i<elementos.length; i++){
+        // Si el botón que se ha seleccionado coincide con el índice que se está recorriendo
+        if(elemento == i){
+            // Si está es invisible entonces mostrarlo
+            if (elementos[i].style.display === "none") {
+                elementos[i].style.display = "block";
+                cualApareceOdesaparece(elemento, 'Aparece');
+            // Si es visible entonces ocultarlo
+            // Esto es para cuando se pincha dos veces seguidas el mismo botón,
+            // para que se oculte si se estaba mostrando, que queda mejor así.
+            } else {
+                elementos[i].style.display = "none";
+                cualApareceOdesaparece(elemento, 'Desaparece');
             }
+        // Si el botón que se ha seleccionado NO coincide con el índice que se está recorriendo
+        // entonces esos elementos se ocultarán siempre.
+        }else{
+            elementos[i].style.display = "none";
         }
+    }
+}
 
-        // Primera versión del toggle invisible-visible
-        // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
-        // function aparecerYdesaparecer(elemento) {
-        //     var x = document.getElementById(elemento);
-        //     if (x.style.display === "none") {
-        //         x.style.display = "block";
-        //         console.log(`Aparece ${elemento}`);
-        //     } else {
-        //         x.style.display = "none";
-        //         console.log(`Desaparece ${elemento}`);
-        //     }
-        // }
+function cualApareceOdesaparece(elemento, queHace){
+    if(elemento == 0){
+        console.log(`${queHace} ${elemento}: iglesia`);
+    }else if(elemento == 1){
+        console.log(`${queHace} ${elemento}: restaurante`);
+    }
+}
