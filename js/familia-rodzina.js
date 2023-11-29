5e530c2c2d43ffa56e6d93f1e03abd8a8afe8103
Paloma Ania Benito <paloma.aniabenito@gmail.com>
2023-11-29 19:45:23 +0100

    initial commit

---
+++ js/familia-rodzina.js
@@ -0,0 +1,91 @@
+console.log('\t✅ enganchado familia-rodzina.js\n\n');
+
+
+// ******************** JAVASCRIPT para cambiar de idioma *******************
+
+function cambiarIdiomaFotos(){
+    let idiomaPagina = document.documentElement.lang;
+    // console.log(`Esta página está en ${idiomaPagina}`);
+    if(idiomaPagina == 'pl'){
+        window.location = './' + 'familia' + '.html';
+    }else if(idiomaPagina == 'es'){
+        window.location = './' + 'rodzina' + '.html';
+    }
+}
+
+// ******************* JAVASCRIPT para cambiar de SECCIÓN *******************
+
+function aparecerYdesaparecer(elemento){
+    let rellenoToggle = document.getElementsByClassName('relleno-toggle');
+    if(elemento == 'iglesia'){
+        rellenoToggle[0].classList.add('display-block');
+        rellenoToggle[0].classList.remove('display-none');
+        rellenoToggle[1].classList.add('display-none');
+        rellenoToggle[1].classList.remove('display-block');
+        rellenoToggle[2].classList.add('display-none');
+        rellenoToggle[2].classList.remove('display-block');
+    }else if(elemento == 'convite'){
+        rellenoToggle[0].classList.add('display-none');
+        rellenoToggle[0].classList.remove('display-block');
+        rellenoToggle[1].classList.add('display-block');
+        rellenoToggle[1].classList.remove('display-none');
+        rellenoToggle[2].classList.add('display-none');
+        rellenoToggle[2].classList.remove('display-block');
+    }else if(elemento == 'fotos'){
+        rellenoToggle[0].classList.add('display-none');
+        rellenoToggle[0].classList.remove('display-block');
+        rellenoToggle[1].classList.add('display-none');
+        rellenoToggle[1].classList.remove('display-block');
+        rellenoToggle[2].classList.add('display-block');
+        rellenoToggle[2].classList.remove('display-none');
+    }
+}
+
+
+
+// ********************** JAVASCRIPT para SECCIÓN FOTOS *********************
+
+function desplazarseFotos(direccion){
+    let imagenes = document.getElementsByClassName('carrousel-fotos-boda');
+    let indiceParaMostrar = 0;
+    for(let i=0; i<imagenes.length; i++){
+        if(imagenes[i].classList.contains('display-block')){
+            if((direccion == 'atras') && (i == 0)){
+                // si el índice es cero, ir a la ´última foto del array
+                indiceParaMostrar = imagenes.length-1;
+            }else if((direccion == 'atras') && (i>0)){
+                indiceParaMostrar = i - 1;
+            }else if((direccion == 'adelante') && (i<imagenes.length-1)){
+                indiceParaMostrar = i + 1;
+            }else if((direccion == 'adelante') && (i == imagenes.length)){
+                // si el índice es cero, ir a la primera foto del array
+                indiceParaMostrar = 0;
+            }
+        }
+    }
+    let pCarrouselFotos = document.getElementById('carrousel-texto');
+    let idiomaPagina = document.documentElement.lang;
+    // console.log(`Esta página está en ${idiomaPagina}`);
+    if(idiomaPagina == 'pl'){
+        pCarrouselFotos.innerHTML = `Wyświetlam zdjęcie ${indiceParaMostrar+1} z ${imagenes.length}`;
+    }else if(idiomaPagina == 'es'){
+        pCarrouselFotos.innerHTML = `Mostrando foto ${indiceParaMostrar+1} de ${imagenes.length}`;
+    }
+    // console.log(indiceParaMostrar);
+    mostrarUnaFotoYocultarElResto(indiceParaMostrar);
+}
+
+function mostrarUnaFotoYocultarElResto(indice){
+    // console.log('entrando en cambiar estilos');
+    let imagenes = document.getElementsByClassName('carrousel-fotos-boda');
+    for(let i=0; i<imagenes.length; i++){
+        if(i == indice){
+            // console.log(`mostrando índice ${i} y ocultando el resto`);
+            imagenes[i].classList.add('display-block');
+            imagenes[i].classList.remove('display-none');
+        }else if(imagenes[i] !== indice){
+            imagenes[i].classList.remove('display-block');
+            imagenes[i].classList.add('display-none');
+        }
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,59 @@
+console.log('\t✅ enganchado el main.js del index.html\n\n');
+
+
+let input = document.getElementById('wage');
+
+
+/*
+    Las siguientes dos funciones son para entrar a una página si
+    cuando pulsemos el botón ACCEDER tras introducir la contraseña
+    correcta en el input.
+    Adaptado de --> https://desarrolloweb.com/articulos/24.php
+*/
+function acceso(){
+    if((input.value == 'familia') || (input.value == 'rodzina')){
+        window.location = `./pages/${input.value}.html`;
+    }else{
+        console.log('Has pulsado la tecla ENTER estando en el input');
+        alert('Contraseña incorrecta\nNiepoprawne hasło');
+    }
+} 
+
+function validate(e) {
+    acceso();
+}
+
+
+/*
+    Función para poder llamar también a la función 'acceso()' si
+    pulsamos la tecla ENTER estando dentro del input.
+    Adaptado de --> https://es.stackoverflow.com/questions/167312/detectar-tecla-intro-en-campo-de-texto
+*/
+function onKeyUp(event) {
+    var keycode = event.keyCode;
+    if(keycode == '13'){
+        if((input.value == 'familia') || (input.value == 'rodzina')){
+            acceso(); 
+        }else{
+            alert('Contraseña incorrecta\nNiepoprawne hasło');
+        }
+    }
+}
+
+
+/*
+    Función para para cambiar de idioma
+*/
+function cambiarIdioma(value){
+    console.log(value);
+    if(value == 'Polski'){
+        document.getElementById('textillo').value =  'Hasło';
+        document.getElementById('boton-acceder').value = 'Dostęp';
+        document.getElementById('img-bandera-idioma').setAttribute('src', './images/bandera-polonia.png');
+    }else if(value == 'Español'){
+        document.getElementById('textillo').value =  'Contraseña';
+        document.getElementById('boton-acceder').value = 'Acceder';
+        document.getElementById('img-bandera-idioma').setAttribute('src', './images/bandera-españa.png');
+    }
+}
+
